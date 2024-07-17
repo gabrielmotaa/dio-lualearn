b89a984de3ba9bd4c8121409726cee0ff9844eb2
Gabriel da Mota <gabrielmotaa@hotmail.com>
2024-07-16 22:05:13 -0300

    Adição de documentação faltante

--- programacao-orientada-a-objetos/game_data.lua
+++ programacao-orientada-a-objetos/game_data.lua
@@ -3,6 +3,7 @@ local class = require("libs.middleclass")
 --- @class GameData
 local GameData = class("GameData")
 
+--- Configura uma instância de 'GameData' para middleclass.
 function GameData:initialize()
   self.activeNode = nil   --- @type Node
   self.isOver = false     --- @type boolean
